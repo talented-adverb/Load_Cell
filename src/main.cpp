72cf252d1d6613f3755b4b8ea82bcf3fbdb96e61
pratham-2004 <ps.pratham04@gmail.com>
2023-11-24 22:59:15 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,59 @@
-// #include<Arduino.h>
-// #include "HX711.h" 
-// #include <SPI.h>
-// #include <SD.h>
+#include<Arduino.h>
+#include "HX711.h" 
+#include <SPI.h>
+#include <SD.h>
 
-// // HX711 circuit wiring
-// #define LOADCELL1_DOUT_PIN 2
-// #define LOADCELL1_SCK_PIN  3
-// // #define LOADCELL2_DOUT_PIN 5
-// // #define LOADCELL2_SCK_PIN  6
-// long reading1;
-// // long reading2;
-// HX711 LoadCell1,LoadCell2;
-// long offset1=1.230;
-// // long offset2=16100;
-// float calibrationfactor1=-16.0376744;
-// // float calibrationfactor2=-21.5541818;
+// HX711 circuit wiring
+#define LOADCELL1_DOUT_PIN 2
+#define LOADCELL1_SCK_PIN  3
+#define LOADCELL2_DOUT_PIN 4
+#define LOADCELL2_SCK_PIN  5
+float reading1;
+float reading2;
+HX711 LoadCell1,LoadCell2;
+float LC1offset1=0.69;
+float LC1offset2=0.55;
+// float LC2offset1=16100
+// float LC2offset2=16100;
+float calibrationfactor1=-21500;
+// float calibrationfactor2=-21.5541818;
 
-// void setup() {
-//   Serial.begin(115200);
-//   Serial.print("Initializing SD card...");
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Initializing SD card...");
 
-//   if (!SD.begin(BUILTIN_SDCARD)) {
-//     Serial.println("Card failed, or not present");
-//     while (1);
-//   }
-//   Serial.println("card initialized.");
-//   LoadCell1.begin(LOADCELL1_DOUT_PIN,LOADCELL1_SCK_PIN);
-//   // LoadCell2.begin(LOADCELL2_DOUT_PIN,LOADCELL2_SCK_PIN);
-// 	// LoadCell1.set_offset(offset1);
-// 	// LoadCell2.set_offset(offset2);
-// }
+  if (!SD.begin(254)) { //BUITLIN_SDCARD-254
+    Serial.println("Card failed, or not present");
+    while (1);
+  }
+  File datafile=SD.open("Datafile4.csv",FILE_WRITE);
+  Serial.println("card initialized.");
+  LoadCell1.begin(LOADCELL1_DOUT_PIN,LOADCELL1_SCK_PIN);
+  LoadCell2.begin(LOADCELL2_DOUT_PIN,LOADCELL2_SCK_PIN);
+}
 
-// void loop() {
-//     String Data1=" ";
-//     String Data2=" ";
-//     reading1 =((LoadCell1.get_value()/calibrationfactor1)/1000)+offset1; //In  Kg
-//     // reading2 =LoadCell2.get_value()/calibrationfactor2;
-//     Data1+=String(reading1);
-//     // Data2+=String(reading2);
-//     File datafile=SD.open("Datalog.txt",FILE_WRITE);
-//     if(datafile){
-//       unsigned long t=millis();
-//       datafile.print(t);
-//       datafile.print("Loadcell1:");
-//       datafile.print(Data1);
-//       datafile.println("Kg");
-//       // datafile.print("Loadcell2:");
-//       // datafile.println(Data2);
-//       Serial.print(t);
-//       Serial.print(":Loadcell1:");
-//       Serial.print(Data1);
-//       Serial.println("Kg");
-//       // Serial.print(" Loadcell2:");
-//       // Serial.println(Data2);
-//       datafile.close();
-//     }
-//     else{
-//       Serial.println("ERROR");
-//       return;
-//     }
-// }
+void loop() {
+    reading1 =LoadCell1.get_value()/calibrationfactor1+LC1offset1+LC1offset2; //In  Kg
+    reading2 =LoadCell2.get_value();
+    File datafile=SD.open("Datalog4.csv",FILE_WRITE);
+    if(datafile){
+      unsigned long t=millis();
+      datafile.print(t);
+      datafile.print(",");
+      datafile.print(reading1);
+      datafile.print(",");
+      datafile.println(reading2);
+      Serial.print(t);
+      Serial.print(",");
+      Serial.print(reading1);
+      Serial.print(",");
+      Serial.println(reading2);
+      datafile.close();
+    }
+    else{
+      Serial.println("ERROR");
+      return;
+    }
+}
 
 
